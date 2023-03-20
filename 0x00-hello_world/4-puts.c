43f506d9413b1e47ea24603311e522de380227fc
AhmedSultan7 <soltanahmed1960.com>
2023-03-20 11:18:11 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
- *hello
- */
+/* This is the main function of the program. It prints
+   "Hello, world!" to the console and returns 0. */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
